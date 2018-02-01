a88431a9719dc3820464faf3edba488d91f9c73b
boldguillermo <g.rocha.o@live.com>
2018-02-01 16:01:50 -0600

    mailchim contact test 1

---
+++ script/script.js
@@ -0,0 +1,6 @@
+$(function() {
+    $(window).load(function() {
+        // https://us10.api.mailchimp.com/2.0/lists/subscribe.json?apikey=<my_api_key>&id=<my_list_id>&email[email]=test@test.com&merge_vars[FNAME]=John&merge_vars[LNAME]=Doe&double_optin=false&send_welcome=false
+    });
+});
+//
