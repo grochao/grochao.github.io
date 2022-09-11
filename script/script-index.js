a384530e68bac40df57bec1639dd545901e865cf
Guillermo Mauricio Rocha Ortiz <g.rocha.o@icloud.com>
2022-09-10 21:27:40 -0600

    canvas 4

--- script/script-index.js
+++ script/script-index.js
@@ -1,10 +1,15 @@
-/*function drawTextAlongArc(context, str, centerX, centerY, radius, angle) {
+function drawTextAlongArc(context, str, centerX, centerY, radius, angle) {
     var len = str.length,
         s;
+    radius = 180;
     context.save();
     context.translate(centerX, centerY);
     context.rotate(-1 * angle / 2);
     context.rotate(-1 * (angle / len) / 2);
+    context.fillStyle = '#ffffff';
+    context.font = '25pt Oxygen';
+
+
     for (var n = 0; n < len; n++) {
         context.rotate(angle / len);
         context.save();
@@ -14,7 +19,7 @@
         context.restore();
     }
     context.restore();
-}*/
+}
 var context;
 
 function AddCircle(context, centerX, centerY, radius) {
@@ -22,30 +27,47 @@ function AddCircle(context, centerX, centerY, radius) {
     context.save();
     context.moveTo(0, 0);
     context.beginPath();
-    context.lineWidth = 3;
+    context.lineWidth = 15;
     context.strokeStyle = '#EFB810';
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-
+    context.fillStyle = 'rgba(255,255,255,0.5)';
+    // context.globalAlpha = 0.5;
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, true);
+    context.fill();
     context.stroke();
     context.restore();
 }
 
-function AddText(context, Text, centerX, centerY) {
+function AddName(context, Text, centerX, centerY) {
 
     context.save();
     context.moveTo(0, 0);
     context.beginPath();
     context.fillStyle = '#0067c6';
-    context.font = '85pt The Nautigal';
+    context.font = '60pt Great Vibes';
     //drawTextAlongArc(context, 'Guillermo Rocha', centerX, centerY, radius, angle);
     context.textAlign = "center";
     context.textBaseline = 'middle';
     var textString = Text;
-    context.fillText(textString, centerX, centerY);
+    context.fillText(textString, centerX, centerY - 10);
     context.stroke();
     context.restore();
 }
 
+function AddLabel(context, Text, centerX, centerY) {
+
+    context.save();
+    context.moveTo(0, 0);
+    context.beginPath();
+    context.fillStyle = '#0067c6';
+    context.font = '25pt Oxygen';
+    //drawTextAlongArc(context, 'Guillermo Rocha', centerX, centerY, radius, angle);
+    context.textAlign = "center";
+    context.textBaseline = 'middle';
+    var textString = Text;
+    context.fillText(textString, centerX, centerY + 50);
+    context.stroke();
+    context.restore();
+}
 
 
 function AddBackGround(context, Text, centerX, centerY) {
@@ -54,8 +76,8 @@ function AddBackGround(context, Text, centerX, centerY) {
     context.moveTo(0, 0);
     context.beginPath();
     drawing = new Image()
-    drawing.src = "https://grochao.github.io/images/bk.png"
-    context.drawImage(drawing, 0, 0, 500, 500);
+    drawing.src = "https://grochao.github.io/images/bk-diagonal.png"
+    context.drawImage(drawing, 0, 0, 600, 600);
     context.stroke();
     context.restore();
 }
@@ -70,8 +92,8 @@ function AddBackGround(context, Text, centerX, centerY) {
         context = canvas.getContext('2d');
         var centerX = canvas.width / 2;
         var centerY = canvas.height / 2;
-        // angle = Math.PI * 0.8,
-        radius = 150;
+        angle = (Math.PI) * 0.8,
+            radius = 200;
 
 
 
@@ -91,8 +113,14 @@ function AddBackGround(context, Text, centerX, centerY) {
 
 
         AddCircle(context, centerX, centerY, radius);
-        AddText(context, "Guillermo", centerX, centerY);
+        AddName(context, "Guillermo Rocha", centerX, centerY);
+        AddLabel(context, "¡Es más nica que el pinol!", centerX, centerY);
         //AddCircle(context, centerX, centerY, 295);
+
+
+        // drawTextAlongArc(context, "¡ES MAS NICA QUE EL PINOL!", centerX, centerY, radius, angle)
+
+
         $btnDescargar = document.querySelector("#btnDescargar");
 
 
