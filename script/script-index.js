010822fdd0628a1a5ac0ce6483bb9a8988fdd71c
Guillermo Mauricio Rocha Ortiz <g.rocha.o@icloud.com>
2022-09-10 22:31:19 -0600

    canvas 5

--- script/script-index.js
+++ script/script-index.js
@@ -27,39 +27,42 @@ function AddCircle(context, centerX, centerY, radius) {
     context.save();
     context.moveTo(0, 0);
     context.beginPath();
-    context.lineWidth = 15;
-    context.strokeStyle = '#EFB810';
+    context.lineWidth = 7;
+    context.strokeStyle = '#d9a404';
     context.fillStyle = 'rgba(255,255,255,0.5)';
     // context.globalAlpha = 0.5;
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, true);
+    context.arc(centerX, centerY, 198, 0, 2 * Math.PI, true);
     context.fill();
     context.stroke();
     context.restore();
 }
 
-function AddName(context, Text, centerX, centerY) {
+function AddName(context, Text, centerX, centerY, size) {
 
     context.save();
     context.moveTo(0, 0);
     context.beginPath();
-    context.fillStyle = '#0067c6';
-    context.font = '60pt Great Vibes';
-    //drawTextAlongArc(context, 'Guillermo Rocha', centerX, centerY, radius, angle);
+    context.fillStyle = '#EFB810';
+    context.font = size + 'pt Great Vibes';
     context.textAlign = "center";
     context.textBaseline = 'middle';
     var textString = Text;
+
+
     context.fillText(textString, centerX, centerY - 10);
+
+
     context.stroke();
     context.restore();
 }
 
-function AddLabel(context, Text, centerX, centerY) {
+function AddLabel(context, Text, centerX, centerY, size) {
 
     context.save();
     context.moveTo(0, 0);
     context.beginPath();
     context.fillStyle = '#0067c6';
-    context.font = '25pt Oxygen';
+    context.font = size + 'pt Oxygen';
     //drawTextAlongArc(context, 'Guillermo Rocha', centerX, centerY, radius, angle);
     context.textAlign = "center";
     context.textBaseline = 'middle';
@@ -76,49 +79,45 @@ function AddBackGround(context, Text, centerX, centerY) {
     context.moveTo(0, 0);
     context.beginPath();
     drawing = new Image()
-    drawing.src = "https://grochao.github.io/images/bk-diagonal.png"
+    drawing.src = "https://grochao.github.io/images/bk-diagonal-flower.png"
     context.drawImage(drawing, 0, 0, 600, 600);
     context.stroke();
     context.restore();
 }
 
 
+function CreateImagen() {
+    var canvas = document.getElementById('myCanvas');
+    context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+    angle = (Math.PI) * 0.8,
+        radius = 200;
+
+    var size_name = ($("#size-nombre").val());
+    var size_label = ($("#size-frase").val());
+    var str_name = $("#name").val();
+    var str_label = $("#frase").val();
+
+    AddBackGround(context, centerX, centerY, );
+
+    var nombre = $.trim($("#name").val());
+    var frase = $.trim($("#frase").val());
+    nombre = nombre == '' ? "Tu Nombre" : nombre;
+    frase = frase == '' ? "Tu Frase" : frase;
+    AddName(context, nombre, centerX, centerY, size_name);
+    AddLabel(context, "¡" + frase + "!", centerX, centerY, size_label);
+
+
+    // drawTextAlongArc(context, "¡ES MAS NICA QUE EL PINOL!", centerX, centerY, radius, angle)
+
+}
 (function($) {
 
     $(window).load(function() {
 
 
-        var canvas = document.getElementById('myCanvas');
-        context = canvas.getContext('2d');
-        var centerX = canvas.width / 2;
-        var centerY = canvas.height / 2;
-        angle = (Math.PI) * 0.8,
-            radius = 200;
-
-
-
-        /*INSERT CIRCLE */
-        // AddCircle(context, centerX, centerY, radius);
-
-        /*INSERT TEXT */
-
-
-
-
-        /*context.fillStyle = "#0067c6";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-*/
-        AddBackGround(context, centerX, centerY, );
-
-
-
-        AddCircle(context, centerX, centerY, radius);
-        AddName(context, "Guillermo Rocha", centerX, centerY);
-        AddLabel(context, "¡Es más nica que el pinol!", centerX, centerY);
-        //AddCircle(context, centerX, centerY, 295);
-
-
-        // drawTextAlongArc(context, "¡ES MAS NICA QUE EL PINOL!", centerX, centerY, radius, angle)
 
 
         $btnDescargar = document.querySelector("#btnDescargar");
@@ -131,14 +130,48 @@ function AddBackGround(context, Text, centerX, centerY) {
             // Crear un elemento <a>
             let enlace = document.createElement('a');
             // El título
-            enlace.download = "Canvas como imagen.png";
+            enlace.download = "pefil.png";
             // Convertir la imagen a Base64 y ponerlo en el enlace
             enlace.href = canvas.toDataURL();
             // Hacer click en él
             enlace.click();
         });
 
-        // context.stroke();
+        $("#slider-vertical-frase").slider({
+            orientation: "Horizontal",
+            range: "min",
+            min: 0,
+            max: 100,
+            value: 25,
+            slide: function(event, ui) {
+                $("#size-frase").val(ui.value);
+                CreateImagen();
+                var g = parseInt(ui.value <= 50 ? 255 : 255 - ((ui.value - 50) * (255 / 50)));
+                var r = parseInt(ui.value >= 50 ? 255 : 255 - ((50 - ui.value) * (255 / 50)));
+                $(".ui-widget-header").css("background-color", "rgb(255,165,0)");
+            }
+        });
+        $("#size-frase").val($("#slider-vertical-frase").slider("value"));
+
+        $("#slider-vertical-nombre").slider({
+            orientation: "Horizontal",
+            range: "min",
+            min: 0,
+            max: 100,
+            value: 60,
+            slide: function(event, ui) {
+                $("#size-nombre").val(ui.value);
+                CreateImagen();
+                var g = parseInt(ui.value <= 50 ? 255 : 255 - ((ui.value - 50) * (255 / 50)));
+                var r = parseInt(ui.value >= 50 ? 255 : 255 - ((50 - ui.value) * (255 / 50)));
+                $(".ui-widget-header").css("background-color", "rgb(255,165,0)");
+            }
+        });
+        $("#size-nombre").val($("#slider-vertical-nombre").slider("value"));
+        CreateImagen();
+        $('body').on('keyup touchend', '.create', function() {
+            CreateImagen();
+        });
     });
 
 
