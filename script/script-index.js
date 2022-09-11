4088d1522d29cafb29f7733187fa4ca30e937a1c
Guillermo Rocha Mauricio Ortiz <g.rocha.o@icloud.com>
2022-09-11 03:58:50 -0600

    Update script-index.js

--- script/script-index.js
+++ script/script-index.js
@@ -50,7 +50,7 @@ function AddName(context, Text, centerX, centerY, size) {
     var textString = Text;
 
 
-    context.fillText(textString, centerX, centerY - 30);
+    context.fillText(textString, centerX, centerY - 80);
 
 
     context.stroke();
