4d6fe524bbce8a3f3af3e52353548e95d454f78e
Guillermo Mauricio Rocha Ortiz <g.rocha.o@icloud.com>
2022-09-11 13:32:48 -0600

    canvas 13

--- script/script-index.js
+++ script/script-index.js
@@ -82,20 +82,34 @@ function AddLabel(context, Text, centerX, centerY, size) {
     context.restore();
 }
 
+var drawing1 = new Image();
+drawing1.crossOrigin = "Anonymous";
+var drawing2 = new Image();
+drawing2.crossOrigin = "Anonymous";
 
 function AddBackGround(context, Text, centerX, centerY) {
 
     context.save();
     context.moveTo(0, 0);
     context.beginPath();
-    drawing = new Image()
-    drawing.crossOrigin = "Anonymous";
+    //drawing = new Image()
+    //drawing.crossOrigin = "Anonymous";
     //drawing.src = "https://grochao.github.io/images/flor.png";
-    //  drawing.src = "images/flor.png";
-    drawing.src = tmp_imagen;
+    //  drawing.src = "images/flor.png"; 
+    // drawing.src = tmp_imagen;
+    /*drawing.onload = function() {
 
+        // context.drawImage(image, 0, 0);
+        
+    };*/
+    if (tmp_imagen == 1) {
 
-    context.drawImage(drawing, 0, 0, 600, 600);
+        context.drawImage(drawing1, 0, 0, 600, 600);
+    } else {
+        context.drawImage(drawing2, 0, 0, 600, 600);
+
+    }
+
     context.stroke();
     context.restore();
 }
@@ -141,7 +155,7 @@ var tmp_size_nombre = 0;
 var tmp_size_frase = 0;
 var tmp_position_frase = 0;
 var tmp_position_nombre = 0;
-var tmp_imagen = 'images/flor.png';
+var tmp_imagen = 1;
 (function($) {
 
     $(window).load(function() {
@@ -156,13 +170,13 @@ var tmp_imagen = 'images/flor.png';
 
         $('body').on('click', '#contenainer-style-background span.ave', function() {
 
-            tmp_imagen = 'images/ave.png';
+            tmp_imagen = 2;
             CreateImagen();
 
         });
         $('body').on('click', '#contenainer-style-background span.flor', function() {
 
-            tmp_imagen = 'images/flor.png';
+            tmp_imagen = 1;
             CreateImagen();
 
         });
@@ -274,11 +288,24 @@ var tmp_imagen = 'images/flor.png';
         $('body').on('keyup touchend', '.create', function() {
             CreateImagen();
         });
+        drawing1.src = "images/flor.png";
+        drawing1.onload = function() {
+
+            drawing2.src = "images/ave.png";
+            drawing2.onload = function() {
+
+                $("body").removeClass('loading');
+                CreateImagen();
+            };
+
+        };
+
+
         ResetSize();
         $(window).resize(function() {
             ResetSize();
         });
-        CreateImagen();
+
     });
 
     function ResetSize() {
