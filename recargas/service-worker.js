403bed7ba77cf3963e9a10fea12c902c35864f04
Guillermo Mauricio Rocha Ortiz <g.rocha.o@icloud.com>
2022-10-21 09:18:11 -0600

    add version 4

--- recargas/service-worker.js
+++ recargas/service-worker.js
@@ -1,7 +1,7 @@
 //self.importScripts('data/games.js');
 
 // Files to cache
-const cacheName = 'update-recargas-app-v2';
+const cacheName = 'update-recargas-app-v3';
 const appShellFiles = [
     '/',
     'img/bk2.jpg',
@@ -62,12 +62,12 @@ self.addEventListener('install', (e) => {
 // Fetching content using Service Worker
 self.addEventListener('fetch', (e) => {
     e.respondWith((async() => {
+
         const r = await caches.match(e.request);
         console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
         if (r) return r;
         const response = await fetch(e.request);
         const cache = await caches.open(cacheName);
-
         console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
         cache.put(e.request, response.clone());
         return response;
